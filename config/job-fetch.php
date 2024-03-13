0340f75c596307c2f91e4f1ffa0ae9f2179d2cc3
imabulhasan99 <imabulhasan99@gmail.com>
2024-03-13 10:38:03 +0600

    add multiple api key and pick a random key from api keys array for every single request

--- config/job-fetch.php
+++ config/job-fetch.php
@@ -11,4 +11,10 @@ return [
     'wordpress_page'=> 8,
     'wordpress_search_query'=> 'Wordpress Developer',
     'apiKey'=> env('JOB_API_KEY','62ed064f9fmsh3bd3f850278b097p133700jsnca750b9876a0'),
+    'apiKeys' => [
+        '4f0d6ab85emsh8e518a3ca23be4bp1fea37jsn61e81932bd06',
+        'ebc33caf94msh3c6170c53ebd4b0p1435e3jsn4a7fd98af219',
+        'ae58fe5e2amshb704d0855b08615p15692djsn631b88e76434',
+        '62ed064f9fmsh3bd3f850278b097p133700jsnca750b9876a0'
+    ]
 ];
